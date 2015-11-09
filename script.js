9e42df1ecb55ae78161f60a94862d84d0d550a72
FreddyCalk <freddycalk@gmail.com>
2015-11-09 16:04:50 -0500

    football game
    added a distance for all six players/plays available to the user… Need
    to add logic to randomly generate the number of yards gained for each
    play

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
 var currYardLine = 20;
-var yardsGained = 15;
 
 $(document).ready(function(){
 		
-	function drawRun(){
+	function drawRun(yards){
 		var canvas = $('#field')[0];
 		var context = canvas.getContext('2d');
 		var currStartYardLine = (currYardLine*6)+88;
-		var newStartYardLine = (6*yardsGained) + currStartYardLine;
+		var newStartYardLine = (6*yards) + currStartYardLine;
 		context.beginPath();
 		context.lineWidth = 5;
 		context.moveTo(currStartYardLine,200);
-		context.lineTo((6*yardsGained)+currStartYardLine,200);
+		context.lineTo((6*yards)+currStartYardLine,200);
 		context.stroke();
 	}
-	function drawPass(){
+	function drawPass(yards){
 		var canvas = $('#field')[0];
 		var context = canvas.getContext('2d');
 		var currStartYardLine = (currYardLine*6)+88;
-		var newStartYardLine = (6*yardsGained) + currStartYardLine;
+		var newStartYardLine = (6*yards) + currStartYardLine;
 		context.beginPath();
 		context.lineWidth = 5;
 		context.moveTo(currStartYardLine,200);
-		context.bezierCurveTo(currStartYardLine,180,currStartYardLine+(6*yardsGained),180,currStartYardLine+(6*yardsGained),200)
+		context.bezierCurveTo(currStartYardLine,180,currStartYardLine+(6*yards),180,currStartYardLine+(6*yards),200)
 		context.stroke();
 
 	}
 	
 $('#big-back').click(function(){
-	drawRun();
-	currYardLine += yardsGained;
+	var yards = 10;
+	drawRun(yards);
+	currYardLine += yards;
+})
+$('#med-back').click(function(){
+	var yards = 7;
+	drawRun(yards);
+	currYardLine += yards;
+})
+$('#small-back').click(function(){
+	var yards = 3;
+	drawRun(yards);
+	currYardLine += yards;
 })
 $('#big-receiver').click(function(){
-	drawPass();
-	currYardLine += yardsGained;
+	var yards = 25;
+	drawPass(yards);
+	currYardLine += yards;
+})
+$('#med-receiver').click(function(){
+	var yards = 15;
+	drawPass(yards);
+	currYardLine += yards;
+})
+$('#small-receiver').click(function(){
+	var yards = 5;
+	drawPass(yards);
+	currYardLine += yards;
 })
 
 })
