c30965a90a3bdbe236584d3670588aae62ada03d
FreddyCalk <freddycalk@gmail.com>
2015-11-09 17:07:48 -0500

    football game
    added the checkTouchdown() function that will add to the home team
    score… Need to figure out a way to simulate the visitor team, or make
    it a two player game… at this point only the home team can possess the
    ball

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var currYardLine = 20;
-
+var bigRunnerEnergy = 100;
+var medRunnerEnergy = 100;
+var smallRunnerEnergy = 100;
+var bigReceiverEnergy = 100;
+var medReceiverEnergy = 100;
+var smallReceiverEnergy = 100;
 $(document).ready(function(){
 		
 	function drawRun(yards){
@@ -8,7 +13,7 @@ $(document).ready(function(){
 		var currStartYardLine = (currYardLine*6)+88;
 		var newStartYardLine = (6*yards) + currStartYardLine;
 		context.beginPath();
-		context.lineWidth = 5;
+		context.lineWidth = 10;
 		context.moveTo(currStartYardLine,200);
 		context.lineTo((6*yards)+currStartYardLine,200);
 		context.stroke();
@@ -19,42 +24,66 @@ $(document).ready(function(){
 		var currStartYardLine = (currYardLine*6)+88;
 		var newStartYardLine = (6*yards) + currStartYardLine;
 		context.beginPath();
-		context.lineWidth = 5;
+		context.lineWidth = 10;
 		context.moveTo(currStartYardLine,200);
 		context.bezierCurveTo(currStartYardLine,180,currStartYardLine+(6*yards),180,currStartYardLine+(6*yards),200)
 		context.stroke();
+	};
+	function checkTouchdown(position){
+		if(position>=100){
+			$('#home-score').text(Number($('#home-score').text())+7);
+			alert('TOUCHDOWN!!!')
+			currYardLine = 20;
+			var canvas = $('#field')[0];
+			var context = canvas.getContext('2d');
+			context.clearRect(0,0,1000,1000);
 
+		}
 	}
 	
-$('#big-back').click(function(){
-	var yards = 10;
-	drawRun(yards);
-	currYardLine += yards;
-})
-$('#med-back').click(function(){
-	var yards = 7;
-	drawRun(yards);
-	currYardLine += yards;
-})
-$('#small-back').click(function(){
-	var yards = 3;
-	drawRun(yards);
-	currYardLine += yards;
-})
-$('#big-receiver').click(function(){
-	var yards = 25;
-	drawPass(yards);
-	currYardLine += yards;
-})
-$('#med-receiver').click(function(){
-	var yards = 15;
-	drawPass(yards);
-	currYardLine += yards;
-})
-$('#small-receiver').click(function(){
-	var yards = 5;
-	drawPass(yards);
-	currYardLine += yards;
-})
+	$('#big-back').click(function(){	
+		var yards = 50;
+		drawRun(yards);
+		currYardLine += yards;
+		checkTouchdown(currYardLine);
+	});
+	$('#med-back').click(function(){
+		var yards = 7;
+		drawRun(yards);
+		currYardLine += yards;
+		checkTouchdown(currYardLine);
+	});
+	$('#small-back').click(function(){
+		var yards = 3;
+		drawRun(yards);
+		currYardLine += yards;
+		checkTouchdown(currYardLine);
+	});
+	$('#big-receiver').click(function(){
+		var yards = 25;
+		drawPass(yards);
+		currYardLine += yards;
+		checkTouchdown(currYardLine);
+	});
+	$('#med-receiver').click(function(){
+		var yards = 15;
+		drawPass(yards);
+		currYardLine += yards;
+		checkTouchdown(currYardLine);
+	});
+	$('#small-receiver').click(function(){
+		var yards = 5;
+		drawPass(yards);
+		currYardLine += yards;
+		checkTouchdown(currYardLine);
+	});
 
-})
+});
+
+
+
+
+
+
+
+
